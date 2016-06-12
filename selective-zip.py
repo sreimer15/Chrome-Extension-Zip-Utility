8382abb14077685bd98ad03a34c462ce51a36777
sreimer15 <sreimer15@gmail.com>
2016-06-12 15:48:57 -0700

    adds functionality to force removal of git folder and of git ignore, fixes bug where temp folder stayed in directory

--- selective-zip.py
+++ selective-zip.py
@@ -3,6 +3,8 @@ import shutil
 import errno
 import sys
 import json
+import os
+import stat
 
 def ignore_function(ignore):
     def _ignore_(path,names):
@@ -12,13 +14,20 @@ def ignore_function(ignore):
         return set(ignored_names)
     return _ignore_
 
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
 def copy_directory(src,dest, directory_to_ignore):
     try:
         if os.path.exists(dest):
             shutil.rmtree(dest)
-        new_directory = shutil.copytree(src, dest + 'temp',ignore = ignore_function(directory_to_ignore, '.git/'))
+        new_directory = shutil.copytree(src, dest + 'temp',ignore = ignore_function([directory_to_ignore, '.git/']))
+        os.remove(dest + 'temp/.gitignore')
+        ## on windows machines our git file is read only, so we add an error call back to force deletion
+        shutil.rmtree(dest + 'temp' + '/.git/', onerror=remove_readonly )
         shutil.make_archive(dest,'zip',dest + 'temp')
-        shutil.rmtree(dest + 'temp')
+        
     except OSError as e:
         if e.errno == errno.ENOTDIR:
             shutil.copy(src,dest)
@@ -27,12 +36,15 @@ def copy_directory(src,dest, directory_to_ignore):
 
 def update_json_file(json_path,prop_to_change, value):
     ## takes path to json file, finds the property and updates the value
-    with open(json_path, 'r+') as f:
-        data = json.load(f)
-        data[prop_to_change] = value
-        f.seek(0)
-        json.dump(data,f,indent = 4)
+    with open(json_path, "r") as jsonFile:
+        data = json.load(jsonFile)
 
+    tmp = data[prop_to_change]
+    data[prop_to_change] = value
+
+    with open("json_path", "w") as jsonFile:
+        jsonFile.write(json.dumps(data))
+        
 parser  = argparse.ArgumentParser(description="zip up a file ignoring certain files")
 
 parser.add_argument("directory_a", help="directory to zip")
@@ -60,6 +72,7 @@ if directory_to_zip:
         else:
             print('Please try again and submit a property to update ')
     copy_directory(directory_to_zip, new_directory_name, directory_to_ignore)
+    shutil.rmtree(new_directory_name + 'temp', onerror=remove_readonly)
     print ('Your new zip file was created!')
 else:
     print ("Please put in a directory name")
